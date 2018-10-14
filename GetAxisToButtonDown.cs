cac5cb3c0f9d263a9301855b5fe8543c7d56aafa
Erendrood <44144950+Erendrood@users.noreply.github.com>
2018-10-14 22:20:04 +0200

    Update GetAxisToButtonDown.cs

--- GetAxisToButtonDown.cs
+++ GetAxisToButtonDown.cs
@@ -1,5 +1,7 @@
 // (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
-// Made by Thore: change anything you like, but if you do, think of the squirrels.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Created by : Thor (Thore) Erendrood
+// Change anything you like, but if you do, think of the squirrels.
 // If you want to store the button presses, find the sister action.
 
 using UnityEngine;
