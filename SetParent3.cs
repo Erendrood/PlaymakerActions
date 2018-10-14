fbbd8867c8a55211e8924ca4cbebd8ef2bca705d
Erendrood <44144950+Erendrood@users.noreply.github.com>
2018-10-14 23:42:42 +0200

    Update SetParent3.cs

--- SetParent3.cs
+++ SetParent3.cs
@@ -1,4 +1,7 @@
-﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Created by Thor (Thore) Erendrood
+
 
 using UnityEngine;
 
