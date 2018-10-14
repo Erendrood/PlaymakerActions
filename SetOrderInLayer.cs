973824e4eba43521ab35811abf816c9bc54a3224
Erendrood <44144950+Erendrood@users.noreply.github.com>
2018-10-14 23:30:27 +0200

    Update SetOrderInLayer.cs

--- SetOrderInLayer.cs
+++ SetOrderInLayer.cs
@@ -1,6 +1,6 @@
 // (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
 /*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
-// Created by Thor (Thore) Erendrood
+// Created by Thor (Thore) Erendrood // http://hutonggames.com/playmakerforum/index.php?topic=19379
 // Any of my actions may be cause of a Guru Medidation.
 
 using UnityEngine;
