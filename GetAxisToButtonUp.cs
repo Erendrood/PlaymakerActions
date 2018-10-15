6220daace2826895d4f1b6032d7920eebc2378e4
Erendrood <44144950+Erendrood@users.noreply.github.com>
2018-10-15 23:24:24 +0200

    Add files via upload

---
+++ GetAxisToButtonUp.cs
@@ -0,0 +1,65 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+// Made by Thore: change anything you like, but if you do, mind the squirrels.
+// If you want to store the button presses, just remove the /* and */ and // below at all the lines that mention the values.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Input)]
+    [Tooltip("Button up, but treats axis input as buttons, i.e. uses Positive Button and Negative Button (e.g. up/down, left/right). There are potentially sibling actions I made, with threshold and additional settings.")]
+    public class GetAxisToButtonUp : FsmStateAction
+    {
+        [RequiredField]
+        [Tooltip("The name of the axis. Set in the Unity Input Manager.")]
+        public FsmString axisName;
+
+        /* 
+        [Tooltip("Set to True if the button is pressed.")]
+        [UIHint(UIHint.Variable)]
+        public FsmBool storePositivePressed;
+
+        [Tooltip("Set to True if the button is pressed.")]
+        [UIHint(UIHint.Variable)]
+        public FsmBool storeNegativePressed; 
+        */
+
+        [ActionSection("Events")]
+        [Tooltip("Event to send if Positive Button is pressed (e.g. right or up).")]
+        public FsmEvent eventPositiveButton;
+
+        [Tooltip("Event to send if the Negative Button is pressed (e.g. down or left).")]
+        public FsmEvent eventNegativeButton;
+
+        public override void Reset()
+        {
+            axisName = "";
+            // storePositivePressed = null;
+            // storeNegativePressed = null;
+        }
+
+        public override void OnUpdate()
+
+        {
+            if (FsmString.IsNullOrEmpty(axisName)) return;
+            var axisValue = Input.GetAxisRaw(axisName.Value);
+
+            if (Input.GetButtonUp(axisName.Value) && Input.GetAxisRaw(axisName.Value) > 0)
+            {
+                Fsm.Event(eventPositiveButton);
+                // storePositivePressed.Value = true;
+                // storeNegativePressed.Value = false;
+            }
+            else if (Input.GetButtonUp(axisName.Value) && Input.GetAxisRaw(axisName.Value) < 0)
+            {
+                Fsm.Event(eventNegativeButton);
+                // storePositivePressed.Value = false;
+                // storeNegativePressed.Value = true;
+            }
+
+
+        }
+
+    } // Class
+} // Namespace
+
